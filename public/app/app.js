577345741c63660f3c98f15684f1baf7e91cb83a
sashimii <stailor@MetrolatorsMBP3.mshome.net>
2015-07-13 08:01:09 -0400

    Starting on Angular App

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,20 @@
 //letters.js Angular App
+
+angular.module('letterApp',[])
+  .controller('letterController', function (){
+    var letter = this;
+    // Info to pass to server
+    letter.info = {
+      firstName: "Rob",
+      lastName: "Singh",
+      email: "rsingh@email.com",
+      options: [ true, false, true ],
+      address: "33 Ashmere Road",
+      city: "Brampton",
+      province: "ON",
+      postalCode: "L7A 1W9",
+      ward: 1
+    }
+
+    
+  });
