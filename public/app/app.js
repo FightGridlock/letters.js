14f897c926f628d16b7980bdba6e58d884ee47b3
sashimii <stailor@Metroland-Administrators-MacBook-Pro-3.local>
2015-07-13 19:42:25 -0400

    Councillor Data Structure

--- public/app/app.js
+++ public/app/app.js
@@ -4,6 +4,7 @@
 angular.module('letterApp',[])
   .controller('letterController', [ '$scope', function (){
     var letter = this;
+    var selectedWard;
     // Info to pass to server
     letter.info = {
       firstName: "",
@@ -13,12 +14,35 @@ angular.module('letterApp',[])
       address: "",
       city: "Brampton",
       province: "ON",
-      postalCode: "L7A 1W9",
-      ward: 1
+      postalCode: "",
+      ward: "1"
+    }
+
+
+    letter.wardList = {
+      "1" : ["1n5", "1 & 5"],
+      "2" : ["2n6", "2 & 6"],
+      "3" : ["3n4", "3 & 4"],
+      "4" : ["3n4", "3 & 4"],
+      "5" : ["1n5", "1 & 5"],
+      "6" : ["2n6", "2 & 6"],
+      "7" : ["7n8", "7 & 8"],
+      "8" : ["7n8", "7 & 8"],
+      "9" : ["9n10", "9 & 10"],
+      "10" : ["9n10", "9 & 10"]
+    }
+
+
+    letter.councillors = {
+      "1n5" : {
+        regional: { name: "Elaine Moore", email: "email@email.com", phone: "416-970-8902" },
+        city: { name: "Grant Gibson", email: "email@email.com", phone: "416-970-8902" }
+      }
     }
 
     letter.setWard = function (wardChosen) {
-      letter.info.ward = wardChosen;
+      letter.info.ward = wardChosen.toString();
+      selectedWard = letter.wardList[letter.info.ward][0];
     }
 
   }]);
