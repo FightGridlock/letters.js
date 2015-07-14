ea74d2fa2385d1fd5d5b3bf35ca681e3908762a5
sashimii <stailor@MetrolatorsMBP3.mshome.net>
2015-07-14 17:52:59 -0400

    Testing $http requests

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('letterApp',[])
-  .controller('letterController', [ '$scope', function (){
+  .controller('letterController', [ '$scope', '$http', function ($scope, $http){
     var letter = this;
     var selectedWard;
     // Info to pass to server
@@ -18,7 +18,7 @@ angular.module('letterApp',[])
       ward: "1"
     }
 
-
+    // Mapping Ward Number to Ward Pair
     letter.wardList = {
       "1" : ["1n5", "1 & 5"],
       "2" : ["2n6", "2 & 6"],
@@ -32,16 +32,39 @@ angular.module('letterApp',[])
       "10" : ["9n10", "9 & 10"]
     }
 
-
+    // List of Councillors
     letter.councillors = {
       "1n5" : {
-        regional: { name: "Elaine Moore", email: "email@email.com", phone: "416-970-8902" },
-        city: { name: "Grant Gibson", email: "email@email.com", phone: "416-970-8902" }
+        regional: { name: "Grant Gibson", email: "email@email.com", phone: "416-970-8902" },
+        city:     { name: "Elaine Moore", email: "email@email.com", phone: "416-970-8902" }
       },
       "2n6" : {
         regional: { name: "Michael Palleschi", email: "email@email.com", phone: "905-495-5559"},
-        city:   { name: "Doug Whillans", email: "email@gmail.com", phone: "905-416-8989"}
+        city:     { name: "Doug Whillans", email: "email@gmail.com", phone: "905-416-8989"}
+      },
+      "3n4" : {
+        regional: { name: "Martin Medeiros" },
+        city:     { name: "Jeff Bowman" }
+      },
+      "7n8" : {
+        regional: { name: "Gael Miles" },
+        city:     { name: "Pat Fortini" }
+      },
+      "9n10" : {
+        regional: { name: "Gurpreet Dhillon" },
+        city:     { name: "John Sprovieri" }
       }
+    }
+
+    letter.testData = "";
+
+    letter.getData = function () {
+      console.log("Starting HTTPGet");
+      $http.get('http://localhost:3000/')
+        .success(function(response){
+          console.log("Success!")
+          letter.testData = response;
+        })
     }
 
     letter.setWard = function (wardChosen) {
