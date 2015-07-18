9aa41ed95fdaa619fcc1f695f5421213b48769a8
Diogo Pinto <h.diogopinto@live.com>
2015-07-18 23:05:56 +0000

    Fixed issue with Auth headers being necessary for all requests

--- app/routes/api.js
+++ app/routes/api.js
@@ -10,16 +10,19 @@ var Ward        = require('../models/ward');
 
 // middleware specific to this router
 router.use(function (req, res, next) {
-    var auth = req.headers['authorization'];  // auth is in base64(username:password)  so we need to decode the base64
-    console.log("Authorization Header is: ", auth);
-    var tmp = auth.split(' ');
-    var buf = new Buffer(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
-    var plain_auth = buf.toString();        // read it back out as a string
-    var creds = plain_auth.split(':');      // split on a ':'
-    var username = creds[0];
-    var password = creds[1];
-    console.log("Username: " + username);
-    console.log("Password: " + password);
+    var auth = req.headers['authorization']; // auth is in base64(username:password)  so we need to decode the base64
+    if (auth)
+    {
+        console.log("Authorization Header is: ", auth);
+        var tmp = auth.split(' ');
+        var buf = new Buffer(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
+        var plain_auth = buf.toString();        // read it back out as a string
+        var creds = plain_auth.split(':');      // split on a ':'
+        var username = creds[0];
+        var password = creds[1];
+        console.log("Username: " + username);
+        console.log("Password: " + password);
+    }
     console.log('Time: ', Date.now());
     next();
 });
